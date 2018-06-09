09e145832228beddbbd207a59c9f0194f39facf8
Tyler J. Cagle <tyler.cagle@colorado.edu>
2018-06-09 19:52:38 -0400

    Initial Commit

---
+++ DriveWayz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DriveWayz
+//
+//  Created by Tyler Jordan Cagle on 6/9/18.
+//  Copyright © 2018 COAD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
